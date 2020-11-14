cbe2a231ca0c6623ba442f1e4e106e8331aeb67b
alsong <alsongdunstan2@gmail.com>
2020-11-14 21:39:26 +0300

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Welcome to best dad jokes we have on the web</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  head () {
+    return {
+      title: 'Welcome To Dad Jokes',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Best place for corny Dad jokes'
+        }
+      ]
+    }
+  }
+}
+</script>
